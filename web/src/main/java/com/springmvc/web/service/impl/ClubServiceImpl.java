e29592297b98c6692db8b9bc6407ae3a05ad7a0b
Azobioz <145475512+Azobioz@users.noreply.github.com>
2024-09-16 17:30:43 +0300

    add commit
    Service repository

---
+++ web/src/main/java/com/springmvc/web/service/impl/ClubServiceImpl.java
@@ -0,0 +1,41 @@
+package com.springmvc.web.service.impl;
+
+import com.springmvc.web.dto.ClubDto;
+import com.springmvc.web.repository.ClubRepository;
+import com.springmvc.web.service.ClubService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.springmvc.web.model.Club;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class ClubServiceImpl implements ClubService {
+
+    private ClubRepository clubRepository;
+
+    public ClubServiceImpl(ClubRepository clubRepository) {
+        this.clubRepository = clubRepository;
+    }
+
+    @Override
+    public List<ClubDto> findAllClubs() {
+         List<Club> clubs = clubRepository.findAll();
+         return clubs.stream().map((club) -> mapToClubDto(club))
+                 .collect(Collectors.toList());
+    }
+
+    private ClubDto mapToClubDto(Club club) {
+        ClubDto clubDto = ClubDto.builder()
+                .id(club.getId())
+                .title(club.getTitle())
+                .photoUrl(club.getPhotoUrl())
+                .content(club.getContent())
+                .createOn(club.getCreateOn())
+                .updateOn(club.getUpdateOn())
+                .build();
+        return clubDto;
+    }
+}
