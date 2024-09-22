7cf1d6514ce5c0635ba7846868d775511dfd6a2b
Aziz <aziz345568667@gmail.com>
2024-09-23 00:38:28 +0300

    Search bar

---
+++ src/main/java/com/springmvc/web/dto/EventDto.java
@@ -0,0 +1,30 @@
+package com.springmvc.web.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventDto {
+
+
+    private Long id;
+    private String name;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private String type;
+    private String photoUrl;
+    private LocalDateTime createdOn;
+    private LocalDateTime updatedOn;
+
+
+
+}
