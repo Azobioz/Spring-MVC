a3dd9b5a3b5b74c21305110ba7ad4a18fda89f78
Aziz <aziz345568667@gmail.com>
2024-09-28 23:07:16 +0300

    Register

--- src/main/java/com/springmvc/web/service/UserService.java
+++ src/main/java/com/springmvc/web/service/UserService.java
@@ -1,9 +1,14 @@
 package com.springmvc.web.service;
 
 import com.springmvc.web.dto.RegistrationDto;
+import com.springmvc.web.model.UserEntity;
+import jakarta.validation.constraints.NotEmpty;
 
 public interface UserService {
 
     void saveUser(RegistrationDto registrationDto);
 
+    UserEntity findByEmail(String email);
+
+    UserEntity findByUsername (String username);
 }
