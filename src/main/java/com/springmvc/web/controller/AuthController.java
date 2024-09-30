1f19d29b927381cd230fe018c04a8c76d3abc6ce
Aziz <aziz345568667@gmail.com>
2024-09-30 10:11:24 +0300

    Login

--- src/main/java/com/springmvc/web/controller/AuthController.java
+++ src/main/java/com/springmvc/web/controller/AuthController.java
@@ -27,20 +27,29 @@ public class AuthController {
         return "register";
     }
 
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
     @PostMapping("/register/save")
     public String register(@Valid @ModelAttribute("user") RegistrationDto user,
                            BindingResult result, Model model) {
+
         UserEntity existingUserEmail = userService.findByEmail(user.getEmail());
+
         if(existingUserEmail != null && existingUserEmail.getEmail() != null && !existingUserEmail.getEmail().isEmpty()) {
-            result.rejectValue("email", "There is already a user with that email/username");
+            return "redirect:/register?fail";
         }
+
         UserEntity existingUserUsername = userService.findByUsername(user.getUsername());
+
         if(existingUserUsername != null && existingUserUsername.getUsername() != null && !existingUserUsername.getUsername().isEmpty()) {
-            result.rejectValue("username", "There is already a user with that email/username");
+            return "redirect:/register?fail";
         }
         if(result.hasErrors()) {
             model.addAttribute("user", user);
-            return "register";
+            return "redirect:/register?fail";
         }
         userService.saveUser(user);
         return "redirect:/clubs?success";
